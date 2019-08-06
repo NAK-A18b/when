188fa5ee6c61822157e584062ad12a0a73d4f7a8
Elias Hühne <elias.huehne@gmail.com>
2019-08-06 19:58:40 +0200

    Check size

--- src/chrome/index.js
+++ src/chrome/index.js
@@ -11,10 +11,12 @@ exports.getDriver = async () => {
     .build();
 };
 
-exports.pageLoad = async (driver) => await driver.wait(until.elementLocated(By.css('input[type="text"]')), 3 * 1000).catch(async error => {
+exports.pageLoad = async (driver) => await driver.wait(until.elementLocated(By.css('input[type="text"]')), 20 * 1000).catch(async error => {
   const elements = await driver.findElements(By.css('img[alt="Scan me!"]')).then(async elements => {
-    console.log("QR-Code for login:");
-    await console.log(await elements[0].getAttribute("src"));
-    process.exit(0);
+    if(elements.size === 1) {
+      console.log(`QR-Code for login:`);
+      await console.log(await elements[0].getAttribute("src"));
+      process.exit(0);
+    }
   });
 });
