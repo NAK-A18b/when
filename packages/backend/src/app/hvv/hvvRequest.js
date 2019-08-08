83bde4e6c88dc5d3b2fd8f2b29ed5f6d71439b31
Elias Hühne <elias.huehne@gmail.com>
2019-08-08 23:11:12 +0200

    HVV-Request now available 🎉

---
+++ packages/backend/src/app/hvv/hvvRequest.js
@@ -0,0 +1,38 @@
+const crypto = require('crypto');
+const request = require('request');
+
+module.exports.hvvRequest = (method, body) => {
+    return new Promise((resolve, reject) => {
+        //Generate hash needed for hvv api server authentication
+        const secret = 'j/o!f$R@a2au';
+        const hash = crypto.createHmac('sha1', secret)
+            .update(body)
+            .digest('base64');
+
+        const options = {
+            url: `https://api-test.geofox.de/gti/public/${method}`,
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8',
+                'geofox-auth-type': 'HmacSHA1',
+                'Connection': 'Keep-Alive',
+                'geofox-auth-user': 'eliashuehne',
+                'geofox-auth-signature': hash
+            },
+            body: body
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(`Request to HVV-API successful with method ${method}`);
+                resolve(body);
+            } else {
+                console.error(`Request to HVV-API failed with response code ${response.statusCode} and body ${body}`);
+                reject(body);
+            }
+        }
+        request(options, callback);
+    });
+
+};
