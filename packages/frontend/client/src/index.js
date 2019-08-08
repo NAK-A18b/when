779d6f620f7f11faf9de9449690a4648f96aad7a
Luis Meyer <luis_meyer@outlook.de>
2019-08-08 11:13:01 +0200

    adds frontend client

---
+++ packages/frontend/client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({ uri: 'localhost:3000/api' });
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<ApolloApp />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
