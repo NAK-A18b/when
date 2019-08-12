ee55bfb80100c3ac502192462ba192df5f58b65b
Elias Hühne <elias.huehne@gmail.com>
2019-08-12 20:32:30 +0200

    Treat start and end as Types and not Scalars

--- packages/frontend/client/src/app/components/create-connection/index.js
+++ packages/frontend/client/src/app/components/create-connection/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
@@ -6,80 +6,85 @@ import TextField from '@material-ui/core/TextField';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 
-import { useMutation } from '@apollo/react-hooks';
+import {useMutation} from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
 import './styles.css';
 
-const baseClassName = 'create-connection'
+const baseClassName = 'create-connection';
 
 const CREATE_CONNECTION = gql`
-  mutation createConnection($start: String!, $end: String!) {
-    createConnection(start: $start, end: $end) {
-      id
-      start
-      end
+    mutation createConnection($start: String!, $end: String!) {
+        createConnection(start: $start, end: $end) {
+            id
+            start
+            {
+                name
+            }
+            end {
+                name 
+            }
+        }
     }
-  }
 `;
 
 const CreateConnection = () => {
-  const [addConnection] = useMutation(CREATE_CONNECTION);
+    const [addConnection] = useMutation(CREATE_CONNECTION);
 
-  const [start, setStart] = useState('');
-  const [end, setEnd] = useState('');
+    const [start, setStart] = useState('');
+    const [end, setEnd] = useState('');
 
-  const handleInput = stateFunc => e => stateFunc(e.target.value);
-  const submitForm = () => {
-    addConnection({
-      variables: {
-        start,
-        end,
-      }
-    }).then( () => {
-      setStart('');
-      setEnd('');
-    })
-  }
+    const handleInput = stateFunc => e => stateFunc(e.target.value);
+    const submitForm = () => {
+        addConnection({
+            variables: {
+                start,
+                end,
+            }
+        }).then(() => {
+            setStart('');
+            setEnd('');
+        })
+    }
 
-  return (
-    <Card className={`${baseClassName}-card`}>
-      <div className={`${baseClassName}-head`}>
-        <Typography variant="h5">
-          Create Connection
-        </Typography>
-      </div>
-      <Divider />
-      <div className={`${baseClassName}-body`}>
-        <div className={`${baseClassName}-body-inputs`}>
-          <div className={`${baseClassName}-body-inputs-textfield`}>
-            <TextField 
-              type='text' 
-              variant="outlined"
-              label='Start Station' 
-              name='name' 
-              value={start} 
-              onChange={handleInput(setStart)}
-            />
-          </div>
-          <span className={`${baseClassName}-arrow`}>&#8594;</span>
-          <div className={`${baseClassName}-body-inputs-textfield`}>
-            <TextField 
-              type='tel'
-              variant="outlined"
-              label='End Station'
-              name='phone'
-              value={end}
-              onChange={handleInput(setEnd)}
-            />
-          </div>
-        </div>
-        <div className={`${baseClassName}-body-button`}>
-          <Button onClick={submitForm} variant="contained" color="primary" >Create</Button>
-        </div>
-      </div>
-    </Card>
-  );
-}
+    return (
+        <Card className={`${baseClassName}-card`}>
+            <div className={`${baseClassName}-head`}>
+                <Typography variant="h5">
+                    Create Connection
+                </Typography>
+            </div>
+            <Divider/>
+            <div className={`${baseClassName}-body`}>
+                <div className={`${baseClassName}-body-inputs`}>
+                    <div className={`${baseClassName}-body-inputs-textfield`}>
+                        <TextField
+                            type='text'
+                            variant="outlined"
+                            label='Start Station'
+                            name='name'
+                            value={start}
+                            onChange={handleInput(setStart)}
+                        />
+                    </div>
+                    <span className={`${baseClassName}-arrow`}>&#8594;</span>
+                    <div className={`${baseClassName}-body-inputs-textfield`}>
+                        <TextField
+                            type='tel'
+                            variant="outlined"
+                            label='End Station'
+                            name='phone'
+                            value={end}
+                            onChange={handleInput(setEnd)}
+                        />
+                    </div>
+                </div>
+                <div className={`${baseClassName}-body-button`}>
+                    <Button onClick={submitForm} variant="contained" color="primary">Create</Button>
+                </div>
+            </div>
+        </Card>
+    );
+};
 
 export default CreateConnection;
