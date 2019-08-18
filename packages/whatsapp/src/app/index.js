5962078f5c0a85552f1e1df5eca3c83bb5f58e0f
Elias Hühne <elias.huehne@gmail.com>
2019-08-18 17:43:48 +0200

    Implement Rest Endpoint

--- packages/whatsapp/src/app/index.js
+++ packages/whatsapp/src/app/index.js
@@ -1,16 +1,30 @@
 'use strict';
 
 const express = require('express');
+const basicAuth = require('express-basic-auth')
+const sulla = require('sulla');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello when\n');
-});
+sulla.create().then(client => {
+  const app = express();
+  app.use(express.json());
+  app.post('/sendMessage', async (req, res) => {
+    const messages = req.body;
+    for (let index in messages) {
+      const message = messages[index];
+      const {receiver, text} = message;
+      await client.sendText(`${receiver}@c.us`, text);
+      const response = {
+        message: "Request received"
+      };
+      res.send(JSON.stringify(response));
+      console.log(`Sent message "${text}" to ${receiver}`);
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+    }
+  });
+  app.listen(PORT, HOST);
+  console.log(`REST-Endpoint up`);
+});
