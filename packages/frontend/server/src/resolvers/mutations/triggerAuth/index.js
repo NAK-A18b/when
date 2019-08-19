661ec19367fe38ab7e8d68c65f79a1ea2b5e31fe
Elias Hühne <elias.huehne@gmail.com>
2019-08-19 20:32:04 +0200

    Validate phone number

--- packages/frontend/server/src/resolvers/mutations/triggerAuth/index.js
+++ packages/frontend/server/src/resolvers/mutations/triggerAuth/index.js
@@ -1,40 +1,60 @@
 const uuid = require('uuid');
+const request = require('request');
+const {parsePhoneNumberFromString} = require('libphonenumber-js/max');
+const {UserInputError} = require("apollo-server-errors");
 
-const { listEntrys } = require('when-aws/dynamodb/actions/list-entrys');
-const  { createEntry } = require('when-aws/dynamodb/actions/create-entry');
+const {listEntrys} = require('when-aws/dynamodb/actions/list-entrys');
+const {createEntry} = require('when-aws/dynamodb/actions/create-entry');
 
 module.exports.randomToken = randomToken = (min = 1000, max = 9999) => {
   return Math.floor(Math.random() * (max - min) + min);
-}
+};
 
 const createUser = async (tel) => {
-  const token = randomToken();
-  // TODO Whatsapp notification with token
-  console.log(token);
-  const params = {
-    TableName: process.env.USER_TABLE,
-    Item: {
-      id: uuid.v1(),
-      tel,
-      token,
-    },
-  };
+  const phoneNumber = parsePhoneNumberFromString(tel, 'DE');
+  if (phoneNumber !== undefined && phoneNumber.isValid() && phoneNumber.getType() === 'MOBILE') {
+    tel = phoneNumber.number.substr(1);
+    const token = randomToken();
 
-  return await createEntry(params);
-}
-
-module.exports.triggerAuth = async (root, { tel }) => {
-  const params = {
-    TableName: process.env.USER_TABLE,
-     ExpressionAttributeValues: {
-      ":t": tel
-     }, 
-     FilterExpression: "tel = :t",
+    request.post('http://35.196.195.229/sendMessage', {
+      json: [{"receiver": tel, "text": `👋 Dein Anmeldecode lautet *${token}* und ist eine Minute lang gültig`}]
+    }, (error, res, body) => {
+      if (error) {
+        console.error(error);
+        return
+      }
+      console.log(`statusCode: ${res.statusCode}`);
+      console.log(body)
+    }).auth('nordakademie', 'T&;2]fX3EN/&v>5', true);
+    console.log(`Created auth code ${token} for ${tel}`);
+    const params = {
+      TableName: process.env.USER_TABLE,
+      Item: {
+        id: uuid.v1(),
+        tel,
+        token,
+      },
+    };
+    return await createEntry(params);
+  } else {
+    throw new UserInputError('', {BAD_PHONE_NUMBER: true});
   }
+
+
+};
+
+module.exports.triggerAuth = async (root, {tel}) => {
+  const params = {
+    TableName: process.env.USER_TABLE,
+    ExpressionAttributeValues: {
+      ":t": tel
+    },
+    FilterExpression: "tel = :t",
+  };
 
   const response = await listEntrys(params);
   const user = response.Items[0];
-  
+
   if (!user) {
     await createUser(tel);
     return true;
@@ -43,13 +63,13 @@ module.exports.triggerAuth = async (root, { tel }) => {
     user.token = newToken;
     // TODO Whatsapp notification with token
     console.log('Generated new Token: ', newToken);
-  
+
     await createEntry({
       TableName: process.env.USER_TABLE,
       Item: user,
     });
-  
+
     return true;
-  }  
+  }
 };
 
