f3e52979e976c9908edfdcc13ae6b1c34708b034
Luis Meyer <luis_meyer@outlook.de>
2019-08-20 17:16:40 +0200

    optimizes login container

--- packages/frontend/client/src/app/constants/index.js
+++ packages/frontend/client/src/app/constants/index.js
@@ -1,4 +1,7 @@
 export const AUTH_TOKEN = 'authentication';
-export const ERRORS = {
-  BAD_PHONE_NUMBER: '☎️ Es scheint als wäre deine Telefonnummer nicht gültig'
-};
+export const ERRORS = [
+  { 
+    name: 'BAD_PHONE_NUMBER',
+    text: '☎️ Es scheint als wäre deine Telefonnummer nicht gültig' 
+  }
+];
