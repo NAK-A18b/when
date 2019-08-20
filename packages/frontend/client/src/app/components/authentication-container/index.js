1c5b98daa554423fd466a12b4894b27c0f2eb4b8
Luis Meyer <luis_meyer@outlook.de>
2019-08-20 23:15:17 +0200

    adds sun and cloud logic

--- packages/frontend/client/src/app/components/authentication-container/index.js
+++ packages/frontend/client/src/app/components/authentication-container/index.js
@@ -1,10 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
-import { withUser } from '../../context/user';
 
 import './styles.css';
 
