12741c090cf322e206ce4e5b59e3b0b7fe50c637
eliashuehne <elias.huehne@gmail.com>
2019-08-21 22:24:56 +0200

    Fix login mask

--- packages/frontend/server/src/resolvers/mutations/triggerAuth/index.js
+++ packages/frontend/server/src/resolvers/mutations/triggerAuth/index.js
@@ -1,54 +1,45 @@
 const request = require('request');
-const { parsePhoneNumberFromString } = require('libphonenumber-js/max');
+const {sendMessage} = require("when-whatsapp/functions/send-message");
+const {parsePhoneNumberFromString} = require('libphonenumber-js/max');
 
-const { UserInputError } = require("apollo-server-errors");
+const {UserInputError} = require("apollo-server-errors");
 
-const { createUser, findUserByTel } = require('../../../entitys/user');
+const {createUser, findUserByTel} = require('../../../entitys/user');
 
-const { createEntry } = require('when-aws/dynamodb/actions/create-entry');
+const {createEntry} = require('when-aws/dynamodb/actions/create-entry');
 
 module.exports.randomToken = randomToken = (min = 1000, max = 9999) => {
-  return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min) + min);
 };
 
-const tokenNotification = ({tel, token}) => {
-  const phoneNumber = parsePhoneNumberFromString(tel, 'DE');
-  if (phoneNumber !== undefined && phoneNumber.isValid() && phoneNumber.getType() === 'MOBILE') {
-    tel = phoneNumber.number.substr(1);
-
+const tokenNotification = (user) => {
+    let tel = user.tel;
+    let token = user.token;
     console.log(`Creating auth code ${token} for ${tel}`);
-    return request.post('http://35.196.195.229/sendMessage', {
-      json: [{"receiver": tel, "text": `👋 Dein Anmeldecode lautet *${token}* und ist eine Minute lang gültig`}]
-    }, (error, res, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-    }).auth('nordakademie', 'T&;2]fX3EN/&v>5', true);
-  } else {
-    throw new UserInputError('', { BAD_PHONE_NUMBER: true });
-  }
-}
+    sendMessage(tel, `👋 Dein Anmeldecode lautet *${token}* und ist eine Minute lang gültig`);
+};
 
-module.exports.triggerAuth = async (root, { tel }) => {
-  const phoneNumber = parsePhoneNumberFromString(tel, 'DE');
-  if (phoneNumber !== undefined && phoneNumber.isValid() && phoneNumber.getType() === 'MOBILE') {
-    tel = phoneNumber.number.substr(1);
+module.exports.triggerAuth = async (root, {tel}) => {
+    const phoneNumber = parsePhoneNumberFromString(tel, 'DE');
+    if (phoneNumber !== undefined && phoneNumber.isValid() && phoneNumber.getType() === 'MOBILE') {
+        tel = phoneNumber.number.substr(1);
 
-    let user = await findUserByTel(tel);
+        let user = await findUserByTel(tel);
 
-    if (!user) {
-      user = await createUser({ tel });
+        if (!user) {
+            user = await createUser({tel});
+        } else {
+            user.token = randomToken();
+            user = await createEntry({
+                TableName: process.env.USER_TABLE,
+                Item: user,
+            });
+        }
+
+        await tokenNotification(user);
+        return true;
     } else {
-      user.token = randomToken();
-      user = await createEntry({
-        TableName: process.env.USER_TABLE,
-        Item: user,
-      });
+        throw new UserInputError('', {BAD_PHONE_NUMBER: true});
     }
-
-    await tokenNotification(user);
-    return true;
-  }
 };
 
