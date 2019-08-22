996a49ff55923f128c02431e1ec0163f24be1acf
Elias Hühne <elias.huehne@gmail.com>
2019-08-22 23:44:42 +0200

    CSSTransition example

--- packages/frontend/client/src/app/pages/login-page/index.js
+++ packages/frontend/client/src/app/pages/login-page/index.js
@@ -5,10 +5,11 @@ import Card from '@material-ui/core/Card';
 import AuthenticationContainer from '../../components/authentication-container';
 import LoginContainer from '../../components/login-container';
 
-import { withUser } from '../../context/user';
-import { ERRORS } from '../../constants';
+import {withUser} from '../../context/user';
+import {ERRORS} from '../../constants';
 
 import './styles.css';
+import CSSTransition from "react-transition-group/CSSTransition";
 
 const baseClassName = 'login-page';
 
@@ -16,11 +17,14 @@ const baseClassName = 'login-page';
 const LoginPage = (props) => {
   const {user} = props;
 
-  const [ clouds, setCloud ] = useState([ 0, 1 ]);
-  const [ auth, setAuth ] = useState(false);
-  const [ error, setError ] = useState('');
-  const [ tel, setTel ] = useState('');
-  const [ token, setToken ] = useState('');
+  const [showButton, setShowButton] = useState(false);
+  const [showMessage, setShowMessage] = useState(true);
+
+  const [clouds, setCloud] = useState([0, 1]);
+  const [auth, setAuth] = useState(false);
+  const [error, setError] = useState('');
+  const [tel, setTel] = useState('');
+  const [token, setToken] = useState('');
 
   const triggerAuth = () => {
     user.triggerAuthentication(tel).then(() => {
@@ -28,71 +32,89 @@ const LoginPage = (props) => {
       setError('');
     }).catch(e => {
       e.graphQLErrors.forEach(gqlError => {
-        const { exception } = gqlError.extensions;
+        const {exception} = gqlError.extensions;
         const error = ERRORS.find(e => exception[e.name]);
-        if (error) { setError(error.text); }
+        if (error) {
+          setError(error.text);
+        }
       })
     });
   };
 
-  const loginUser = () => user.login(tel, parseInt(token));
+  const loginUser = () => {
+    if(user.login(tel, parseInt(token))) {}
+    setShowMessage(false);
+  };
   const removeCloud = (index) => () => setCloud(clouds.filter(c => c !== index));
 
   return (
-    <Card className={`${baseClassName}-card`}>
-      <section className="stage">
-        <div className="sun"></div>
-        { clouds.includes(0) &&
-          <div onClick={removeCloud(0)} className="cloud one"></div> 
-        }
-        { clouds.includes(1) &&
-          <div onClick={removeCloud(1)} className="cloud two"></div> 
-        }
-        <div className="train">
-          <div className="wagon">
-            <div className="window-left"></div>
-            <div className="window-left"></div>
-            <div className="window-right"></div>
-            <div className="window-right"></div>
-          </div>
-          <div className="wagon">
-            <div className="window-left"></div>
-            <div className="window-left"></div>
-            <div className="window-right"></div>
-            <div className="window-right"></div>
-          </div>
-          <div className="wagon">
-            <div className="window-left"></div>
-            <div className="window-left"></div>
-            <div className="window-right"></div>
-            <div className="window-right"></div>
-          </div>
-          <div className="locomotive">
-            <div className="cabin"></div>
-            <div className="motor"></div>
-            <div className="chimney">
-              <div className="smoke"></div>
+      !showButton ? (
+      <Card className={`${baseClassName}-card`}>
+        <section className="stage">
+          <div className="sun"></div>
+          {clouds.includes(0) &&
+          <div onClick={removeCloud(0)} className="cloud one"></div>
+          }
+          {clouds.includes(1) &&
+          <div onClick={removeCloud(1)} className="cloud two"></div>
+          }
+          <div className="train">
+            <div className="wagon">
+              <div className="window-left"></div>
+              <div className="window-left"></div>
+              <div className="window-right"></div>
+              <div className="window-right"></div>
             </div>
-            <div className="light"></div>
+            <div className="wagon">
+              <div className="window-left"></div>
+              <div className="window-left"></div>
+              <div className="window-right"></div>
+              <div className="window-right"></div>
+            </div>
+            <div className="wagon">
+              <div className="window-left"></div>
+              <div className="window-left"></div>
+              <div className="window-right"></div>
+              <div className="window-right"></div>
+            </div>
+            <div className="locomotive">
+              <div className="cabin"></div>
+              <div className="motor"></div>
+              <div className="chimney">
+                <div className="smoke"></div>
+              </div>
+              <div className="light"></div>
+            </div>
           </div>
+        </section>
+        <CSSTransition in={showMessage} timeout={1000} classNames="login-page-wrapper" unmountOnExit
+                       onEnter={() => setShowButton(false)}
+                       onExited={() => setShowButton(true)}>
+          <div className={`${baseClassName}-wrapper`}>
+            <div className={`${baseClassName}-slider-wrapper${auth ? '--auth' : ''}`}>
+              <LoginContainer
+                  tel={tel}
+                  setTel={setTel}
+                  disabled={auth}
+                  submitCallback={triggerAuth}
+              />
+              <AuthenticationContainer
+                  token={token}
+                  setToken={setToken}
+                  submitCallback={loginUser}
+              />
+            </div>
+          </div>
+        </CSSTransition>
+        {showButton && (
+            <h1></h1>
+        )}
+      </Card>
+      ) : (<Card className={`${baseClassName}-card`}>
+        <div className={`${baseClassName}-wrapper-exit-active`}>
+          Test
         </div>
-      </section>
-      <div className={`${baseClassName}-wrapper`}>
-        <div className={`${baseClassName}-slider-wrapper${auth ? '--auth' : ''}`}>
-          <LoginContainer
-            tel={tel}
-            setTel={setTel}
-            disabled={auth}
-            submitCallback={triggerAuth}
-          />
-          <AuthenticationContainer
-            token={token}
-            setToken={setToken}
-            submitCallback={loginUser}
-          />
-        </div>
-      </div>
-    </Card>
+      </Card>)
   )
 }
 
@@ -119,4 +141,5 @@ export default withUser(LoginPage);
           }
         </div>
       </Card>
-    </div> */}
+    </div> */
+}
