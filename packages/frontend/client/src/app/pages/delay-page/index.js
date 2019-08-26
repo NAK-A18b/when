564a4c78091f2610a45c6a6343d75623eb8a04aa
Elias Hühne <elias.huehne@gmail.com>
2019-08-26 21:28:37 +0200

    Fix margin

--- packages/frontend/client/src/app/pages/delay-page/index.js
+++ packages/frontend/client/src/app/pages/delay-page/index.js
@@ -10,6 +10,7 @@ const DelayPage = () => {
     <div>
       <Heading title="Delays" subtitle="0 reported"/>
       <img src="images/undraw_subway_7vh7.svg"/>
+      <span className="no-delays">All trains are on time</span>
     </div>
   );
 }
