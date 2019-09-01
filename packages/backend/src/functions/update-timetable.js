fa29cabd5e88637147e09535c39f0fcaedd6d8f0
Luis Meyer <luis_meyer@outlook.de>
2019-09-01 22:01:15 +0200

    fixes code style and minor bugs

--- packages/backend/src/functions/update-timetable.js
+++ packages/backend/src/functions/update-timetable.js
@@ -1,32 +1,34 @@
-const {getTimes} = require( '../app/nordakademie');
+const { getTimes } = require('../app/nordakademie');
 
-const {createEntry} = require('when-aws/dynamodb/actions/create-entry');
-const {listEntrys} = require('when-aws/dynamodb/actions/list-entrys');
+const { createEntry } = require('when-aws/dynamodb/actions/create-entry');
+const { listEntrys } = require('when-aws/dynamodb/actions/list-entrys');
 
 module.exports.updateTimetable = async event => {
-    const centurias = (await listEntrys({
-        TableName: process.env.CENTURIA_TABLE
-    })).Items;
+  const centurias = (await listEntrys({
+    TableName: process.env.CENTURIA_TABLE
+  })).Items;
 
-    if (!centurias) return;
-    return Promise.all(centurias.map( async (centuria) => {
-        const { name, semester } = centuria;
-    
-        const times = await getTimes(name, semester);
-        let params = {
-            TableName: process.env.TIMETABLE_TABLE,
-            Item: {
-                centuria: name,
-            },
-        };
-    
-        if (times.start !== undefined) {
-            params.Item.start = times.start;
-        }
-        if (times.end !== undefined) {
-            params.Item.end = times.end;
-        }
+  if (!centurias) return;
+  return Promise.all(
+    centurias.map(async centuria => {
+      const { name, semester } = centuria;
 
-        return await createEntry(params);
-    }))
+      const times = await getTimes(name, semester, true);
+      let params = {
+        TableName: process.env.TIMETABLE_TABLE,
+        Item: {
+          centuria: name
+        }
+      };
+
+      if (times.start) {
+        params.Item.start = times.start;
+      }
+      if (times.end) {
+        params.Item.end = times.end;
+      }
+
+      return await createEntry(params);
+    })
+  );
 };
