bda6e193b65afec04ec9aa0063b28b1d18fcf0db
Luis Meyer <luis_meyer@outlook.de>
2019-11-20 21:17:49 +0100

    add run-all command and more

--- packages/frontend/server/src/resolvers/mutations/triggerAuth/index.js
+++ packages/frontend/server/src/resolvers/mutations/triggerAuth/index.js
@@ -1,38 +1,50 @@
-const {sendMessage} = require("when-whatsapp/send-message");
-const {parsePhoneNumberFromString} = require('libphonenumber-js/max');
+const { sendMessage } = require('when-whatsapp/send-message');
+const { parsePhoneNumberFromString } = require('libphonenumber-js/max');
 
-const {UserInputError} = require("apollo-server-errors");
+const { UserInputError } = require('apollo-server-errors');
 
-const { createUser, findUserByTel, updateUser } = require('../../../entitys/user');
+const {
+  createUser,
+  findUserByTel,
+  updateUser
+} = require('../../../entitys/user');
 
 module.exports.randomToken = randomToken = (min = 1000, max = 9999) => {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
-const tokenNotification = (user) => {
-    let tel = user.tel;
-    let token = user.token;
-    console.log(`Creating auth code ${token} for ${tel}`);
-    return sendMessage(tel, `👋 Dein Anmeldecode lautet *${token}* und ist eine Minute lang gültig`);
+const tokenNotification = user => {
+  let tel = user.tel;
+  let token = user.token;
+  console.log(`Creating auth code ${token} for ${tel}`);
+  sendMessage(
+    tel,
+    `👋 Dein Anmeldecode lautet *${token}* und ist eine Minute lang gültig`
+  );
 };
 
 module.exports.triggerAuth = async (root, { tel }) => {
-    const phoneNumber = parsePhoneNumberFromString(tel, 'DE');
-    if (phoneNumber !== undefined && phoneNumber.isValid() && phoneNumber.getType() === 'MOBILE') {
-        tel = phoneNumber.number.substr(1);
-        let user = await findUserByTel(tel);
+  const phoneNumber = parsePhoneNumberFromString(tel, 'DE');
+  if (
+    phoneNumber &&
+    phoneNumber.isValid() &&
+    phoneNumber.getType() === 'MOBILE'
+  ) {
+    tel = phoneNumber.number.substr(1);
+    let user = await findUserByTel(tel);
 
-        if (!user) {
-            user = await createUser({ tel });
-        } else {
-            user.token = randomToken();
-            user = await updateUser(user);
-        }
-
-        await tokenNotification(user).catch(e => console.error(e));
-        return tel;
+    if (!user) {
+      user = await createUser({ tel });
     } else {
-        throw new UserInputError('Error on AuthTrigger', { BAD_PHONE_NUMBER: true });
+      user.token = randomToken();
+      user = await updateUser(user);
     }
-};
 
+    tokenNotification(user);
+    return tel;
+  } else {
+    throw new UserInputError('Error on AuthTrigger', {
+      BAD_PHONE_NUMBER: true
+    });
+  }
+};
